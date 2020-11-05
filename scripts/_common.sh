1117ae494f140dab0e7f2bab90203c2da3ceb441
ericgaspar <junk.eg@free.fr>
2020-11-05 17:28:45 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,7 +8,7 @@
 YNH_PHP_VERSION="7.3"
 
 # dependencies used by the app
-extra_php_dependencies="php${YNH_PHP_VERSION}-zip"
+extra_php_dependencies="php${YNH_PHP_VERSION}-zip php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-curl"
 
 #=================================================
 # PERSONAL HELPERS
