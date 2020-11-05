0d26ea8c2018d4ea2bbfea16004a0892bb6de453
ericgaspar <junk.eg@free.fr>
2020-11-05 15:31:29 +0100

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,8 +5,10 @@
 # COMMON VARIABLES
 #=================================================
 
+YNH_PHP_VERSION="7.3"
+
 # dependencies used by the app
-pkg_dependencies=""
+extra_php_dependencies="php${YNH_PHP_VERSION}-zip"
 
 #=================================================
 # PERSONAL HELPERS
